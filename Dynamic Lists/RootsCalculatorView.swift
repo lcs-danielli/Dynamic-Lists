e1b0c2d9771ec5c7c64b9095d8071ac415b0f549
Daniel Li <danli@lcs.on.ca>
2025-01-14 11:45:15 -0500

    finish editing result

--- Dynamic Lists/RootsCalculatorView.swift
+++ Dynamic Lists/RootsCalculatorView.swift
@@ -12,7 +12,19 @@ struct RootsCalculatorView: View {
     @State var a: Double = 0.0
     @State var b: Double = 0.0
     @State var c: Double = 0.0
-    
+    var result: String{
+        
+        let discriminant = b * b - 4 * a * c
+        
+        if discriminant < 0{
+            return "No real roots"
+        } else {
+            let x1 = ( b * -1 - discriminant.squareRoot() ) / (2 * a)
+            let x2 = ( b * -1 - discriminant.squareRoot() ) / (2 * a)
+            
+            return "x \(x1.formatted(.number.precision(.fractionLength(2)))) and x ≈ \(x2.formatted(.number.precision(.fractionLength(2))))"
+        }
+    }
     var body: some View {
         NavigationStack{
             VStack {
