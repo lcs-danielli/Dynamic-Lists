88f9418467c62f4a6ae66b797fc9f65fa3d13bcc
Daniel Li <danli@lcs.on.ca>
2025-01-14 11:05:50 -0500

    Initial Commit

---
+++ Dynamic Lists/Dynamic_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_ListsApp.swift
+//  Dynamic Lists
+//
+//  Created by 李泽宇 on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
